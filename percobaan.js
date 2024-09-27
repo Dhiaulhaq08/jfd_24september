2e6bb9c0ed5a9ff87d8ad350e04808846dba0a3b
Dhiaulhaq08 <dhiaulhaq11@gmail.com>
2024-09-27 21:38:02 +0700

    edit console.log dan nambah komen

--- percobaan.js
+++ percobaan.js
@@ -1,6 +1,11 @@
+//memanggil modil bawaan dari node.js yaitu http
+//untuk membuat server http
 const http = require('http')
 
 http.createServer(function(request, response ){
     response.writeHead(200, {'Content-type' : 'text/plain'})
     response.end('Halo guys, script ini di gnerate dari backend node,js')
 }).listen(3000, function(){   console.log ('server sudah jalan, buka http://localhosnnodet:3000')} )
+
+// tes nambah komentar
+// kirim ke 2x
