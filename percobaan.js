f196da610541870ad633fb8ca4a365f0bc33feea
Dhiaulhaq08 <dhiaulhaq11@gmail.com>
2024-09-30 19:46:10 +0700

    mendeeteksi halaman atau url web

--- percobaan.js
+++ percobaan.js
@@ -1,6 +1,26 @@
 const http = require('http')
 
 http.createServer(function(request, response ){
-    response.writeHead(200, {'Content-type' : 'text/plain'})
-    response.end('Halo guys, script ini di gnerate dari backend node,js')
-}).listen(3000, function(){   console.log ('server sudah jalan, buka http://localhosnnodet:3000')} )
+    
+    response.writeHead(200, {'Content-type' : 'text/html'})
+    if (request.url == '/') {
+        response.end(`<h1>Halo guys, script ini di gnerate dari backend node js</h1>`)
+    
+
+    }
+    else if (request.url == '/profil'){
+        response.end(
+            `<ul>
+                <li> nama lengkap : dhiaulhaq</li>
+                <li> nama panggilan : dhia </li>
+                <li> alamat: bandung</li>
+                <li> status : mahasiswa</li>
+            </ul>`
+        )
+    }
+    else { 
+        response.end(`<h1>303 : Halaman tidak ditemukan<hr></h1>`)
+    }
+    
+  
+}).listen(3000, function(){console.log ('server sudah jalan, buka http://localhosnnodet:3000')} )
