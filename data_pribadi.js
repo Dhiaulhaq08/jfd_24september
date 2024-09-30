9c02568b3979c479f08e3f7bff15c0688e235e19
Dhiaulhaq08 <dhiaulhaq11@gmail.com>
2024-09-30 21:39:49 +0700

    ignore dan install modul

---
+++ data_pribadi.js
@@ -0,0 +1,9 @@
+let nama_depan = 'Levi'
+let nama_belakang = 'Ackerman'
+let nama_lengkap = nama_depan + ' ' + nama_belakang
+nama_panggilan = 'mikasa'
+
+module.exports = { 
+    nama_lengkap,nama_depan,
+    alamat: 'bandung',
+}
---
+++ percobaan2.js
@@ -0,0 +1,10 @@
+// syarat menggunakan variabel / object yang ada di file lain
+//1. panggil dulu filenya
+//2.file yang dipanggil sudah mengskpor varaiblenya
+//3>panggil file variabel
+
+const dp = require('./data_pribadi.js')
+console.log(dp.nama_lengkap)
+console.log(dp.nama_depan)
+console.log(nama_panggilan)
+console.log(dp.alamat)
