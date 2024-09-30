37989c11540caa7b259914ae36751eb4276525c0
Dhiaulhaq08 <dhiaulhaq11@gmail.com>
2024-09-30 20:19:12 +0700

    menambah objek pada tab baru

--- percobaan.js
+++ percobaan.js
@@ -11,14 +11,33 @@ http.createServer(function(request, response ){
 
     }
     else if (request.url == '/profil'){
+        let tahun_lahir = 1980
+        let tahun_ini = 2024
+        let umur = tahun_ini - tahun_lahir 
         response.end(
             `<ul>
                 <li> nama lengkap : dhiaulhaq</li>
                 <li> nama panggilan : dhia </li>
                 <li> alamat: bandung</li>
                 <li> status : mahasiswa</li>
+                <li> tanggal lahir: 17 Agustus ${tahun_lahir}</li>
+                <li> umur : ${umur} tahun </li> 
             </ul>`
         )
+    }
+    else if (request.url == '/hubungi-saya'){
+        let kontak = { 
+            wa :'021021021021',
+            ig : "igugu",
+        }
+        
+        response.end(
+            `<ul>
+                <li>wa : ${kontak.wa}</li>
+                <li>ig : ${kontak.ig}</li>
+            </ul>`
+            
+)  
     }
     else { 
         response.end(`<h1>303 : Halaman tidak ditemukan<hr></h1>`)
